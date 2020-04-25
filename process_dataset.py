77aea96ba43c443617ea5e9a015f8f890e2b8300
Tao <2761564455@qq.com>
2020-04-25 17:00:28 +0800

    修改process_datasets.py文件

--- process_dataset.py
+++ process_dataset.py
@@ -23,7 +23,7 @@ with open('category.txt','w') as f:
 dict_categories = {}
 for i, category in enumerate(categories):
     dict_categories[category] = i
-
+print(dict_categories)
 files_input = ['%s-validation.csv'%dataset_name,'%s-train.csv'%dataset_name]
 files_output = ['val_videofolder.txt','train_videofolder.txt']
 for (filename_input, filename_output) in zip(files_input, files_output):
@@ -34,14 +34,16 @@ for (filename_input, filename_output) in zip(files_input, files_output):
     for line in lines:
         line = line.rstrip()
         items = line.split(';')
-        folders.append(items[0])
-        idx_categories.append(os.path.join(dict_categories[items[1]]))
+        folders.append(items[0]) #视频名字是用数字命名的
+        idx_categories.append(dict_categories[items[1]])
+    print("idx_categories:",idx_categories)
+    print("folders:",folders)
     output = []
     for i in range(len(folders)):
         curFolder = folders[i]
         curIDX = idx_categories[i]
         # counting the number of frames in each video folders
-        dir_files = os.listdir(os.path.join('20bn-%s'%dataset_name, curFolder))
+        dir_files = os.listdir(os.path.join('/home/aistudio/data/data30061/20bn-%s'%dataset_name, curFolder))
         output.append('%s %d %d'%(curFolder, len(dir_files), curIDX))
         print('%d/%d'%(i, len(folders)))
     with open(filename_output,'w') as f:
